4f9c30fbcd1db11647a187839a02751f4d11f697
DESKTOP-52ONACO\xxxpx <23071170@siswa.um.edu.my>
2024-06-08 15:48:00 +0800

    Modify homepage & footer design, Add link to header

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -17,33 +17,36 @@
     <div class = "container">
         <div class = "navbar">
             <div class = "logo">
-                <a href = "index.html"><img src = "img/mealmate_logo1.png" alt = "meal time logo" width = "125px"></a>
+                <a href = "/"><img src = "img/mealmate_logo1.png" alt = "Meal Mate logo" width = "125px"></a>
             </div>
     
             <nav>
                 <ul>
-                    <li><a href = "#">ABOUT</a></li>
-                    <li><a href = "#">MENUS</a></li>
-                    <li><a href = "#">MY CART</a></li>
+                    <li><a href = "/about">ABOUT</a></li>
+                    <li><a href = "/menus">MENUS</a></li>
+                    <li><a href = "/cart">MY CART</a></li>
                 </ul>
             </nav>
         </div>
 
     </div>
     </div>
+
         @yield('content')
 
    <!--==== footer ====-->
    <div class = "footer">
         <div class = "container">
             <div class = "row">
-                <div class = "footer-col-2">
-                    <img src = "img/mealmate_logo2.png">
-                    <p>Copyright 2024 Meal Mate</p>
-                </div>
+
                 <div class = "footer-col-1">
                     <h3>Download our app</h3>
                     <p>Download app for android an ios</p>
+                </div>
+
+                <div class = "footer-col-2">
+                    <img src = "img/mealmate_logo2.png">
+                    <p>Copyright 2024 Meal Mate</p>
                 </div>
                 
                 <div class = "footer-col-3">
@@ -55,6 +58,7 @@
                         <li>Join Us</li>
                     </ul>
                 </div>
+                
                 <div class = "footer-col-4">
                     <h3>Follow Us</h3>
                     <ul>
